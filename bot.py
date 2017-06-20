a40227fc877509141d7c0a214ce89c41b6f3dcb1
ChrisKaspersky <fckthscty@gmail.com>
2017-06-20 13:43:20 +0300

    Фильтрация
    Добавлена фильтрация повторной отправки

--- bot.py
+++ bot.py
@@ -43,16 +43,11 @@ except vk.exceptions.VkAuthError: # Если ошибка заключается
 
 log("Начат приём сообщений")
 
-last_message = api.messages.get(out=0, count=1)
+last_message = ''
 while True:
-    print(last_message)
-    print(api.messages.get(out=0, count=1))
-    if last_message[1]['body'] != api.messages.get(out=0, count=1)[1]['body']:
-        last_message = api.messages.get(out=0, count=1)
-        #user=users.get(user_ids=last_message[1]['uid'])['first_name']
-        log("принято сообщение: " + last_message[1]['body'])
-
-        api.messages.send(user_id=last_message[1]['uid'], message=answer(last_message[1]['body'])) #Отправка ответа
-        time.sleep(1)
-    time.sleep(1)
+    last_message=api.messages.get(out=0,count=1)
+    the_lastest_message=api.messages.getHistory(count=1, user_id=last_message[1]['uid'])
+    if (the_lastest_message[1]['body']==last_message[1]['body']) and (the_lastest_message[1]['uid']!=434145659):
+        api.messages.send(user_id=last_message[1]['uid'], message=answer(last_message[1]['body']))
+    time.sleep(2)
 
