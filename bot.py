da17b62094610741cbd4d73cbcaed03051d40c6a
S1eap <S_leap@mail.ru>
2017-06-21 11:26:04 +0300

    Переписал отправку
    Переписал отправку, навсегда (вроде) устранил проблему двойных сообщений (у меня не работал твой фильтр), временно убрал всяческий вывод информации (в том числе и логи), устранил ошибку повторного ответа на сообщение после перезапуска бота

--- bot.py
+++ bot.py
@@ -1,58 +1,44 @@
 import logging #Библиотека для ведения логов, потом пригодится
 import vk.exceptions #Модуль с описанием исключений связанных с API
 import time
-from datetime import datetime
 
 logging.basicConfig(format='[# %(levelname)-10s [%(asctime)s]  %(message)s', level=logging.INFO)
 
-tokenfile = open('token.txt','r')
+tokenfile=open('token.txt','r')
+#/////logss=open('loggs.txt','w')
 
-quest = ['Привет','привет','Как дела?','как дела?','Как дела','как дела']
-ans = ['Привет','Привет','Хорошо, как у тебя?','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя']
-
-
-def log(information): #Функция для вывода информации в консоль и автоматической дозаписи лога
-    logs = open('logs.txt', 'a')
-    logging.info(information)
-    time = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M")
-    writable_information = time + ": " + information.lower() +'\n'
-    logs.write(writable_information)
-    logs.close()
-
+quest=['Привет','привет','Как дела?','как дела?','Как дела','как дела']
+ans=['Привет','Привет','Хорошо, как у тебя?','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя','Хорошо, как у тебя']
 
 def answer(vopr):
-    for i in range (0,len(quest)+1):
-        if i==len(quest):
-            break
-        if vopr==quest[i]:
-            return ans[i]
-    if i==len(quest):
-        return 'Я не знаю такого вопроса'
+	for i in range (0,len(quest)+1):
+		if i==len(quest):
+			break
+		if vopr==quest[i]:
+			return ans[i]
+	if i==len(quest):
+		return 'Я не знаю такого вопроса'
 
-
-log("Сервис запущен") # Вывод инфы по авторизации
-log("Авторизация...")
+logging.info("Сервис запущен") # Вывод инфы по авторизации
+logging.info("Авторизация...")
 token=tokenfile.readline()
 
 try:
     session = vk.Session(access_token=token)
     api = vk.API(session) #Создаём обьект класса API
 except vk.exceptions.VkAuthError: # Если ошибка заключается в авторизации
-        log('Авторизация не удалась: неверный токен')
-        raise vk.exceptions.AUTHORIZATION_FAILED
+        logging.info('Авторизация не удалась: неверный логин или пароль')
 
-log("Начат приём сообщений")
-
-last_message = api.messages.get(out=0, count=1)
-while True:
-    print(last_message)
-    print(api.messages.get(out=0, count=1))
-    if last_message[1]['body'] != api.messages.get(out=0, count=1)[1]['body']:
-        last_message = api.messages.get(out=0, count=1)
-        #user=users.get(user_ids=last_message[1]['uid'])['first_name']
-        log("принято сообщение: " + last_message[1]['body'])
-
-        api.messages.send(user_id=last_message[1]['uid'], message=answer(last_message[1]['body'])) #Отправка ответа
-        time.sleep(1)
-    time.sleep(1)
+logging.info("Начат приём сообщений")
+previous_message='et68n957tve7r8tvrn'
+last_message = ''
+while True:	
+													#*
+	last_message=api.messages.get(out=0,count=1)
+	the_lastest_message=api.messages.getHistory(count=1, user_id=last_message[1]['uid'])
+	if (the_lastest_message[1]['body']==last_message[1]['body']) and (the_lastest_message[1]['uid']!=434145659):
+		api.messages.send(user_id=last_message[1]['uid'], message=answer(last_message[1]['body'])) #Отправка ответа
+	time.sleep(2)
+	
+												#*
 
