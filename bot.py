1d93b867906d61e20bf79af2a77f8fd4104bf56f
S1eap <S_leap@mail.ru>
2017-06-21 18:39:49 +0300

    Переделал код под табуляцию

--- bot.py
+++ bot.py
@@ -8,32 +8,34 @@ from datetime import datetime
 logging.basicConfig(format='[# %(levelname)-10s [%(asctime)s]  %(message)s', level=logging.INFO)
 
 tokenfile = open('token.txt','r')
-
+noanswer=open('noanswers.txt','w')
 quests={'привет':'Привет.','дела':'Хорошо, как у тебя?'}
 
 def finder(message):
-    message = message.replace('?','')
-    answer = ""
-    message=message.split(' ')
-    l=len(message)
-    for word in message:
-        try:
-            answer=answer + ' ' + quests[word]
-        except KeyError:
-            continue
-        if answer == "":
-            return 'И все таки я такого не знаю'
-    return answer
+	message2 = message
+	message = message.replace('?','')
+	answer = ""
+	message=message.split(' ')
+	l=len(message)
+	for word in message:
+		try:
+			answer=answer + ' ' + quests[word]
+		except KeyError:
+			continue
+		if answer == "":
+			noanswer.write(message2+'\n')
+			return 'И все таки я такого не знаю'
+	return answer
 	
 
 
 def log(information): #Функция для вывода информации в консоль и автоматической дозаписи лога
-    logs = open('logs.txt', 'a')
-    logging.info(information)
-    time = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M")
-    writable_information = time + ": " + information.lower() +'\n'
-    logs.write(writable_information)
-    logs.close()
+	logs = open('logs.txt', 'a')
+	logging.info(information)
+	time = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M")
+	writable_information = time + ": " + information.lower() +'\n'
+	logs.write(writable_information)
+	logs.close()
 
 
 log("Сервис запущен") # Вывод инфы по авторизации
@@ -41,38 +43,38 @@ log("Авторизация...")
 token=tokenfile.readline()
 
 try:
-    session = vk.Session(access_token=token)
-    api = vk.API(session) #Создаём обьект класса API
-    log("Соединение установлено")
+	session = vk.Session(access_token=token)
+	api = vk.API(session) #Создаём обьект класса API
+	log("Соединение установлено")
 except vk.exceptions.VkAuthError: # Если ошибка заключается в авторизации
-        log("Авторизация не удалась: неверный токен")
-        raise vk.exceptions.AUTHORIZATION_FAILED
+	log("Авторизация не удалась: неверный токен")
+	raise vk.exceptions.AUTHORIZATION_FAILED
 
 log("Начат приём сообщений")
 
 last_message = ''
 while True:
-    try:
-        last_message=api.messages.get(out=0,count=1)
-        last_message[1]['body']=last_message[1]['body'].lower()
-        the_lastest_message=api.messages.getHistory(count=1, user_id=last_message[1]['uid'])
-        the_lastest_message[1]['body']=the_lastest_message[1]['body'].lower()
+	try:
+		last_message=api.messages.get(out=0,count=1)
+		last_message[1]['body']=last_message[1]['body'].lower()
+		the_lastest_message=api.messages.getHistory(count=1, user_id=last_message[1]['uid'])
+		the_lastest_message[1]['body']=the_lastest_message[1]['body'].lower()
         
         
-        if (the_lastest_message[1]['body']==last_message[1]['body']) and (the_lastest_message[1]['uid']!=434145659):
-            api.messages.send(user_id=last_message[1]['uid'], message=finder(last_message[1]['body']))
-            sender = api.users.get(user_ids=last_message[1]['uid'])[0]['first_name'] + ' ' + api.users.get(user_ids=last_message[1]['uid'])[0]['last_name']
-            log('('+ sender + ')' + "Сообщение принято: " + last_message[1]['body'])
-            log('('+ sender + ')' + "Сообщение отправлено: " + finder(last_message[1]['body']))
-        time.sleep(2)
+		if (the_lastest_message[1]['body']==last_message[1]['body']) and (the_lastest_message[1]['uid']!=434145659):
+			api.messages.send(user_id=last_message[1]['uid'], message=finder(last_message[1]['body']))
+			sender = api.users.get(user_ids=last_message[1]['uid'])[0]['first_name'] + ' ' + api.users.get(user_ids=last_message[1]['uid'])[0]['last_name']
+			log('('+ sender + ')' + "Сообщение принято: " + last_message[1]['body'])
+			log('('+ sender + ')' + "Сообщение отправлено: " + finder(last_message[1]['body']))
+		time.sleep(2)
         
         
-    except requests.packages.urllib3.exceptions.ReadTimeoutError or requests.exceptions.ReadTimeout:
-        log("Соединение разорвано, попытка подключения")
-        try:
-            session = vk.Session(access_token=token)
-            api = vk.API(session)  # Создаём обьект класса API
-            log("Соединение установлено")
-        except vk.exceptions.VkAuthError:  # Если ошибка заключается в авторизации
-            log("Авторизация не удалась: неверный токен")
-            raise vk.exceptions.AUTHORIZATION_FAILED
+	except requests.packages.urllib3.exceptions.ReadTimeoutError or requests.exceptions.ReadTimeout:
+		log("Соединение разорвано, попытка подключения")
+		try:
+			session = vk.Session(access_token=token)
+			api = vk.API(session)  # Создаём обьект класса API
+			log("Соединение установлено")
+		except vk.exceptions.VkAuthError:  # Если ошибка заключается в авторизации
+			log("Авторизация не удалась: неверный токен")
+			raise vk.exceptions.AUTHORIZATION_FAILED
